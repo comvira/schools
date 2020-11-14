2aaf2764111110a9dd1a19da6629acde7d261c3e
comvira <anfilatov@outlook.com>
2020-11-14 10:42:56 +0300

    test

--- src/main/java/homework1/Test.java
+++
@@ -1,7 +0,0 @@
-package homework1;
-
-public class Test{
-    public static void main(String[] args) {
-        System.out.println("Proba");
-    }
-}
--- src/test/java/homework1Test/TestTest.java
+++
@@ -1,7 +0,0 @@
-package homework1Test;
-
-public class TestTest{
-    public static void main(String[] args) {
-        System.out.println("Proba");
-    }
-}
