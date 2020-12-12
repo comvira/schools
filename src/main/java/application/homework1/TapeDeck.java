1923a2845981544ee31ad18b5e20b0dfdb952da5
oem <afilatov75@gmail.com>
2020-12-12 10:49:47 +0300

    TapeDeck

---
+++ src/main/java/application/homework1/TapeDeck.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class TapeDeck {
+        boolean canRecord = false;
+        void playTape() {
+            System.out.println("пленка проигрьюается");
+        }
+        void recordTape() {
+            System.out.println("идет запись на пленку");
+        }
+}
