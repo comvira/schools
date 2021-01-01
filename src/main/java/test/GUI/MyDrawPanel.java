db04ccbe2dbb6a9431cd1e5ce64ba0c8734d6517
oem <afilatov75@gmail.com>
2021-01-01 12:23:18 +0300

    MiniMusicApp

--- src/main/java/test/GUI/MyDrawPanel.java
+++ src/main/java/test/GUI/MyDrawPanel.java
@@ -1,20 +1,35 @@
-/*
 package GUI;
 
+import javax.sound.midi.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class MyDrawPanel extends JPanel {
+public class MyDrawPanel extends JPanel implements ControllerEventListener {
+    boolean msg = false;
+
+    public void controlChange(ShortMessage event) {
+        msg = true;
+        repaint();
+    }
+
     public void paintComponent(Graphics g) {
-        g.fillRect(0, 0, this.getWidth () , this.getHeight ()) ;
+        if (msg) {
+            Graphics2D g2 = (Graphics2D) g;
 
-        int red = (int) (Math.random() * 255);
-        int green = (int) (Math.random() * 255);
-        int blue = (int) (Math.random() * 255);
+            int r = (int) (Math.random() * 250);
+            int gr = (int) (Math.random() * 250);
+            int b = (int) (Math.random() * 250);
 
-        Color randomColor = new Color(red, green, blue);
-        g.setColor(randomColor);
-        g.fillOval(70,70,100,100);
+            g. setColor (new Color (r, gr, b)) ;
+
+            int ht = (int) ((Math.random() * 120) + 10);
+            int width = (int) ((Math.random() * 120) + 10);
+
+            int x = (int) ((Math.random() * 40) + 10);
+            int y = (int) ((Math.random() * 40) + 10);
+
+            g.fillRect(x, y, ht, width) ;
+            msg = false;
+        }
     }
 }
-*/
--- src/main/java/test/homework1/MiniMiniMusicApp.java
+++ src/main/java/test/homework1/MiniMiniMusicApp.java
@@ -1,35 +1,52 @@
 package homework1;
 
+import GUI.MyDrawPanel;
+
 import javax.sound.midi.*;
+import javax.swing.*;
 
 public class MiniMiniMusicApp implements ControllerEventListener{
+    static JFrame f = new JFrame("Первый клип");
+    static MyDrawPanel ml;
+
     public static void main(String[] args) {
         MiniMiniMusicApp mini = new MiniMiniMusicApp();
         mini.play();
         //System.exit(0);
     }
 
+    public void setUpGui() {
+        ml = new MyDrawPanel();
+        f.setContentPane(ml);
+        f.setBounds(30,30,300,300);
+        f.setVisible(true);
+    }
+
     public void play() {
+        setUpGui();
+
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
 
-            int[] eventsIWand = {127};
-            sequencer.addControllerEventListener(this, eventsIWand);
+            sequencer.addControllerEventListener(ml, new int[] {127});
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
+            int r = 0;
+
             for (int i = 5; i < 61; i+= 4) {
+                r = (int) ((Math.random() * 50) + 1);
+
                 track.add(makeEvent(176, 1,127, 0, i));
+                track.add(makeEvent(144,1,r,100,i));
 
-                track.add(makeEvent(144,1,i,100,i));
-
-                track.add(makeEvent(128,1,i,100,i + 2));
+                track.add(makeEvent(128,1,r,100,i + 2));
             }
 
             sequencer.setSequence(seq);
-            sequencer.setTempoInBPM(220);
+            sequencer.setTempoInBPM(120);
             sequencer.start();
 
 
