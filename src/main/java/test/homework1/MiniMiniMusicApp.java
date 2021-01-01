c457197ef3ae487706b9a0f3f67da79ef880b29e
oem <afilatov75@gmail.com>
2021-01-01 09:08:52 +0300

    MiniMusicApp

--- src/main/java/test/homework1/MiniMiniMusicApp.java
+++ src/main/java/test/homework1/MiniMiniMusicApp.java
@@ -10,34 +10,36 @@ public class MiniMiniMusicApp {
 
     public void play() {
         try {
-            Sequencer player = MidiSystem.getSequencer();
-            player.open();
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
-            ShortMessage first = new ShortMessage();
-            first. setMessage(192, 1, 102, 0) ;
-            MidiEvent changeInstrument = new MidiEvent (first, 1) ;
-            track.add(changeInstrument);
+            for (int i = 5; i < 61; i+= 4) {
+                track.add(makeEvent(144,1,i,100,i));
+                track.add(makeEvent(128,1,i,100,i + 2));
+            }
 
-            ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, 44, 100);
-            MidiEvent noteOn = new MidiEvent(a, 1);
-            track.add(noteOn);
-
-
-
-            ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
-            MidiEvent noteOff = new MidiEvent(b, 16);
-            track.add(noteOff);
-
-            player.setSequence(seq);
-            player.start();
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static MidiEvent makeEvent (int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent (a, tick) ;
+        } catch (Exception ex) {
+            //ex.printStackTrace();
+        }
+        return event;
     }
 }
