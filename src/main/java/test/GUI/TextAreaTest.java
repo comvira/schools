90345cd41489bc4274975d0025e0692ab732e2d7
oem <afilatov75@gmail.com>
2021-01-04 14:13:37 +0300

    JTextAreaTest

---
+++ src/main/java/test/GUI/TextAreaTest.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TextAreaTest {
+    JTextArea text;
+
+    public static void main(String[] args) {
+        TextAreaTest gui = new TextAreaTest();
+        gui.go();
+    }
+
+    public void go() {
+
+    }
+}
