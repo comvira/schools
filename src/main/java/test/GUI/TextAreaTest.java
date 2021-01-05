99e36952f761828795b93e687570e6963a4c7265
oem <afilatov75@gmail.com>
2021-01-05 08:34:30 +0300

    JTextAreaTest

--- src/main/java/test/GUI/TextAreaTest.java
+++ src/main/java/test/GUI/TextAreaTest.java
@@ -16,7 +16,28 @@ public class TextAreaTest {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(606, 600);
+        JPanel panel = new JPanel();
+
+        JButton button = new JButton("Click me");
+        button.addActionListener(this::actionPerformed);
+
+        text = new JTextArea(10, 20);
+        text.setLineWrap(true);
+
+        JScrollPane scrollPane = new JScrollPane(text);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        panel.add(scrollPane);
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+
+        frame.setSize(350, 300);
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ev) {
+        text.append("button clicked \n");
     }
 }
